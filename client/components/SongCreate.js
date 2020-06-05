9df493f8211aff83b6c81aaf3ab14fd033a73b39
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-06-05 10:58:36 +0300

    navigate back after submit

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -12,16 +13,18 @@ class SongCreate extends Component {
   onSubmit(event) {
     event.preventDefault();
     // console.log(this.props)
+
     this.props.mutate({
       variables: {
         title: this.state.title
       }
-    });
+    }).then(() => hashHistory.push('/'));
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a new song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
