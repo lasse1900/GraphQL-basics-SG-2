2d0e7b7bb397aa94a08eb58c0f760818e5be3786
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-06-09 10:01:22 +0300

    added likes counter

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
   onLike(id) {
-    console.log(id)
+    this.props.mutate({ variables: { id } })
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i
-            className="material-icons"
-            onClick={() => this.onLike(id)}
-          >
-            thumb_up</i>
+          <div className="vote-box">
+            {likes}
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id)}
+            >
+              thumb_up
+            </i>
+          </div>
         </li>
       );
     });
@@ -29,4 +35,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
